3b643d1bb7d458670ac61352b44306cb9f0f9111
Anastatia Prusakova <anprusakova@gmail.com>
2024-12-09 16:21:26 +0300

    hw18 исправлены замечания: добавлены анотации для enum и jsonb

--- src/main/java/ru/prusakova/mdm/model/MdmMessage.java
+++ src/main/java/ru/prusakova/mdm/model/MdmMessage.java
@@ -1,10 +1,9 @@
 package ru.prusakova.mdm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import ru.prusakova.mdm.dto.MdmMessagePayload;
 import ru.prusakova.mdm.dto.enums.EventTypeEnum;
 
@@ -21,10 +20,14 @@ public class MdmMessage extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
     private UUID externalId;
+
     private String guid;
+
+    @Enumerated(value = EnumType.STRING)
     private EventTypeEnum type;
+
+    @JdbcTypeCode(SqlTypes.JSON)
     private MdmMessagePayload payload;
-
-
 }
--- src/main/java/ru/prusakova/mdm/model/MdmMessageOutbox.java
+++ src/main/java/ru/prusakova/mdm/model/MdmMessageOutbox.java
@@ -1,10 +1,9 @@
 package ru.prusakova.mdm.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 import ru.prusakova.mdm.dto.MdmMessageResponse;
 import ru.prusakova.mdm.dto.enums.DeliveryStatusEnum;
 import ru.prusakova.mdm.dto.enums.TargetEnum;
@@ -22,9 +21,16 @@ public class MdmMessageOutbox extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private UUID mdmMessageId;
+
+    @Enumerated(value = EnumType.STRING)
     private DeliveryStatusEnum status;
+
+    @Enumerated(value = EnumType.STRING)
     private TargetEnum target;
+
+    @JdbcTypeCode(SqlTypes.JSON)
     private MdmMessageResponse responseData;
 
 

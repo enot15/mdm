e38d76a98309b3cd8b0f795374d9fe9c9cf24817
Anastatia Prusakova <anprusakova@gmail.com>
2024-12-24 12:03:33 +0300

    hw19 добавлена ошибка о том что валидация не прошла

--- src/main/java/ru/prusakova/mdm/listener/MdmChangePhoneConsumer.java
+++ src/main/java/ru/prusakova/mdm/listener/MdmChangePhoneConsumer.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.prusakova.mdm.dto.MdmChangePhoneEvent;
+import ru.prusakova.mdm.exception.MdmException;
 import ru.prusakova.mdm.util.JsonUtil;
 
 @Slf4j
@@ -23,9 +24,14 @@ public class MdmChangePhoneConsumer {
     public void consume(ConsumerRecord<String, String> consumerRecord) {
         log.info("Событие из кафки получен: {}", consumerRecord.toString());
         try {
-            MdmChangePhoneEvent response = jsonUtil.fromJson(consumerRecord.value(), MdmChangePhoneEvent.class);
-            validator.validate(response)
-                    .forEach(it -> log.warn("{} в поле {}", it.getMessage(), it.getPropertyPath()));
+            MdmChangePhoneEvent response = jsonUtil.fromJson(consumerRecord.value(), MdmChangePhoneEvent.class);;
+            if (!validator.validate(response).isEmpty()) {
+                validator.validate(response)
+                        .forEach(it -> log.warn("{} в поле {}", it.getMessage(), it.getPropertyPath()));
+                throw new MdmException("Сообщение не прошло валидацию");
+            }
+        } catch (MdmException e) {
+            log.error("Сообщение не прошло валидацию", e);
         } catch (Exception e) {
             log.error("Ошибка преобразования JSON: {}", consumerRecord.value(), e);
         }
